53a59a09e7f970f4349ad8cddcd15140d8c2e767
rysiek98 <52510889+rysiek98@users.noreply.github.com>
2020-09-19 23:18:09 +0200

    Change language of game & change startDialog

--- XorO/app/src/main/java/com/example/xoro/MainActivity.kt
+++ XorO/app/src/main/java/com/example/xoro/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.xoro
 import android.app.AlertDialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -17,15 +19,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Full screen
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                // Set the content to appear under the system bars so that the
-                // content doesn't resize when the system bars hide and show.
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                // Hide the nav bar and status bar
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        fullScreen()
 
         btExit.setOnClickListener{
             finish()
@@ -38,9 +32,11 @@ class MainActivity : AppCompatActivity() {
         }
 
             btStartGame.setOnClickListener {
+
                 val dialogView = LayoutInflater.from(this).inflate(R.layout.start_dialog, null)
                 val mBuilder = AlertDialog.Builder(this).setView(dialogView)
                 val mAlert = mBuilder.show()
+                mAlert.window?.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
                 dialogView.rgAIvsPlayer.setOnCheckedChangeListener { group, checkedId ->
                             val gameTypeData: RadioButton = dialogView.findViewById(checkedId)
                     dialogView.rgDifficlutlevel.setOnCheckedChangeListener { group, checkedId ->
@@ -56,9 +52,28 @@ class MainActivity : AppCompatActivity() {
                 }
                 dialogView.btBack.setOnClickListener {
                     mAlert.dismiss()
+                    fullScreen()
                 }
             }
     }
+
+    override fun onResume() {
+        super.onResume()
+        fullScreen()
+    }
+
+    fun fullScreen(){
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
 }
 
 
